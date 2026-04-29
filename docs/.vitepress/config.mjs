4a3de2394dc1dc3a9bec507067d0453e68f52cda
manee_too <denzidiscord96@gmail.com>
2026-04-29 21:33:36 +0700

    Update config.mjs

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -7,9 +7,9 @@ export default defineConfig({
   description: "All-in-One, Fast & Buffer-Based Networking for Roblox",
   themeConfig: {
     logo: {
-      light: '/git-assets/Wisp-Logo.png',
-      dark: '/git-assets/Wisp-Logo.png',
-      alt: 'Wisp Logo'
+      light: '/assets/Wisp-Logo.png',
+      dark: '/assets/Wisp-Logo.png',
+      alt: 'WispLogo'
     },
 
     search: {
